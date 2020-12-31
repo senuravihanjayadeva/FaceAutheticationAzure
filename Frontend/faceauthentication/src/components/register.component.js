ca0661afe590585270be4911973485141270411f
SenuraJayadeva <senurajayadeva@gmail.com>
2020-12-31 20:44:26 +0530

    send image to firebase and add face to LargeFaceList

--- Frontend/faceauthentication/src/components/register.component.js
+++ Frontend/faceauthentication/src/components/register.component.js
@@ -7,20 +7,124 @@ import Progress from "./Progress";
 export default function RegisterComponent() {
   //states for webcam
   const webcamRef = React.useRef(null);
-  const [imgSrc, setImgSrc] = React.useState(null);
+  const [imgSrc, setImgSrc] = React.useState(
+    "https://www.clker.com/cliparts/Z/k/z/B/Q/d/blue-silhouette-man-hi.png"
+  );
+
+  //states for send image to firebase
+
+  const [imageURL, setImageURL] = useState("");
+  const [uploadPercentage, setuploadPercentage] = useState(0);
 
   //method for capture an image
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
+    //console.log(imageSrc);
   }, [webcamRef, setImgSrc]);
 
+  function uploadImage(e) {
+    e.preventDefault();
+
+    if (imgSrc !== null) {
+      alert("clicked");
+      const fileName = Math.floor(Math.random() * 100000 + 1) + ".jpg";
+      const uploadTask = storage
+        .ref(`faceauth/${fileName}`)
+        .putString(imgSrc, "data_url", { contentType: "image/jpeg" });
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          //progress function
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setuploadPercentage(progress);
+        },
+        (error) => {
+          //error function
+          console.log(error);
+        },
+        () => {
+          //complete function
+          storage
+            .ref("faceauth")
+            .child(fileName)
+            .getDownloadURL()
+            .then((urlFirebase) => {
+              console.log("Image Url is = " + urlFirebase);
+              setImageURL(urlFirebase);
+
+              const config = {
+                headers: {
+                  "Content-Type": "application/json",
+                  "Ocp-Apim-Subscription-Key":
+                    "a680691db6174916bb8819e75475a406",
+                },
+              };
+
+              const newImageDetails = {
+                url: urlFirebase,
+              };
+
+              axios
+                .post(
+                  "https://eastus.api.cognitive.microsoft.com/face/v1.0/largefacelists/hexalist/persistedfaces?detectionModel=detection_01",
+                  newImageDetails,
+                  config
+                )
+                .then((response) => {
+                  console.log(
+                    "Response for LargeFaceList is = " +
+                      response.data.persistedFaceId
+                  );
+                  alert("Successfully Added to Large Face List");
+                })
+                .catch((err) => {
+                  alert(err.message);
+                });
+            });
+        }
+      );
+    } else {
+      alert("First You Must Select An Image");
+    }
+  }
+
   return (
-    <div>
+    <div className="container">
       <h1>Register</h1>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
-      <button onClick={capture}>Capture photo</button>
-      {imgSrc && <img src={imgSrc} />}
+      <div className="row">
+        <div className="col-md-6">
+          <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+          <button onClick={capture}>Capture photo</button>
+        </div>
+        <div className="col-md-6">
+          {" "}
+          {imgSrc && (
+            <>
+              {" "}
+              <div class="form-group">
+                <img src={imgSrc} style={{ width: "80%" }} />{" "}
+              </div>
+              <br />
+              <div class="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Your Name"
+                />
+              </div>
+              <br />
+              <div class="form-group">
+                <Progress percentage={uploadPercentage} />
+              </div>
+              <br />
+              <button onClick={uploadImage}>Register</button>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
